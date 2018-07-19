2a39dc358a47f952ab64ec6c0564ba0ab2b28ae7
Troy <troy@troywiegand.com>
2018-07-19 17:01:11 -0400

    Added help command and other Daveyisms

--- app.js
+++ app.js
@@ -6,12 +6,44 @@ client.on('ready', ()=>{
     console.log('ONLINE')
 })
 
+food = (message) =>{
+    const random=Math.floor(Math.random() * 6)
+    if(random===0||random===1)
+    message.channel.send('TWAMWICHES!!')
+    if(random===2)
+    message.channel.send('pancakes')
+    if(random===3)
+    message.channel.send('protein cookie')
+    if(random===4)
+    message.channel.send('pastaaaa')
+    if(random===5)
+    message.channel.send('chicken and rice')
+}
+
+help = (message) =>{
+    message.channel.send('Hello, I\'m DaveyBot. \n Here\'s a list of my commands: \n \n d!food ------ recieve a message of one of DaveyBot\'s fave foods')
+}
+
 let prefix = 'd!'
 client.on('message', message=>{
     if(message.author===client.user) return
     if(message.content.startsWith(prefix + "food")){
-        message.channel.sendMessage('TWAMWICHES!!')
+        food(message)
     }
+    if(message.content.startsWith(prefix + "help")){
+        help(message)
+    }
+    if(message.content.includes('fuck')){
+        message.channel.send('Don\'t you cuss at me!')   
+    }
+
+    if(message.content.includes('jesus') || message.content.includes('Jesus')){
+        message.channel.send('You can just call me DaveyBot')   
+    }
+
 })
+
+
+
 
 client.login(settings.token)
